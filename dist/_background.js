278132828eef4ee5df2b2de80ee9421ae1703f0b
SanujaLW <sanujalw@gmail.com>
2020-03-11 18:01:19 +0530

    Added dist to git

---
+++ dist/_background.js
@@ -0,0 +1,26 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                  new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { hostEquals: '' }
+                  })
+                ],
+                actions: [ new chrome.declarativeContent.ShowPageAction() ]
+            }
+        ]);
+    });
+    chrome.runtime.onConnect.addListener(function(port) {
+        if(port.name === "did-siop"){
+            port.onMessage.addListener(function(msg) {
+                if(msg.scheme === 'openid://' && msg.scope.includes('openid') && msg.scope.includes('did_authn')){
+                    port.postMessage("openid ok");
+                    port.postMessage(msg);
+                }
+            });
+        }
+    });
+});
+},{}]},{},[1]);
