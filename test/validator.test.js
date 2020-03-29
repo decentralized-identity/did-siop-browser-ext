6117e8f46c23b517e0dc9a513e88ea547dee51b3
SanujaLW <sanujalw@gmail.com>
2020-03-29 07:35:55 +0530

    Added test resource file

--- test/validator.test.js
+++ test/validator.test.js
@@ -1,88 +1,64 @@
 const validator = require('../src/common/validator');
 const nock = require('nock');
+const resources = require('./validator.test.resources');
 
-const jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJpYXQiOjE1ODQxMDA4MTksImlzcyI6ImRpZDpldGhyOjB4QjA3RWFkOTcxN2I0NEI2Y0Y0MzljNDc0MzYyYjlCMDg3N0NCQkY4MyIsInJlc3BvbnNlX3R5cGUiOiJpZF90b2tlbiIsImNsaWVudF9pZCI6Imh0dHBzOi8vbXkucnAuY29tL2NiIiwic2NvcGUiOiJvcGVuaWQgZGlkX2F1dGhuIiwic3RhdGUiOiJhZjBpZmpzbGRraiIsIm5vbmNlIjoibi0wUzZfV3pBMk1qIiwicmVzcG9uc2VfbW9kZSI6ImZvcm1fcG9zdCIsInJlZ2lzdHJhdGlvbiI6eyJqd2tzX3VyaSI6Imh0dHBzOi8vdW5pcmVzb2x2ZXIuaW8vMS4wL2lkZW50aWZpZXJzL2RpZDpldGhyOjB4QjA3RWFkOTcxN2I0NEI2Y0Y0MzljNDc0MzYyYjlCMDg3N0NCQkY4Mzt0cmFuc2Zvcm0ta2V5cz1qd2tzIiwiaWRfdG9rZW5fc2lnbmVkX3Jlc3BvbnNlX2FsZyI6WyJFUzI1NksiLCJFZERTQSIsIlJTMjU2Il19fQ.LI6KBAAVAdLRL4pfYPYbRcz1pKJ5WKVxgUp7eXntC1J6OT1WqegFqdgONshNIXXdULUhxy1C0paXc85Kgd9bWA'
-const jwt_uri = 'http://localhost/requestJWT';
-const queryObj = {
-    response_type: 'id_token',
-    client_id: 'https://rp.example.com/cb',
-    scope: 'openid did_authn',
-    request: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJpYXQiOjE1ODQxMDA4MTksImlzcyI6ImRpZDpldGhyOjB4QjA3RWFkOTcxN2I0NEI2Y0Y0MzljNDc0MzYyYjlCMDg3N0NCQkY4MyIsInJlc3BvbnNlX3R5cGUiOiJpZF90b2tlbiIsImNsaWVudF9pZCI6Imh0dHBzOi8vbXkucnAuY29tL2NiIiwic2NvcGUiOiJvcGVuaWQgZGlkX2F1dGhuIiwic3RhdGUiOiJhZjBpZmpzbGRraiIsIm5vbmNlIjoibi0wUzZfV3pBMk1qIiwicmVzcG9uc2VfbW9kZSI6ImZvcm1fcG9zdCIsInJlZ2lzdHJhdGlvbiI6eyJqd2tzX3VyaSI6Imh0dHBzOi8vdW5pcmVzb2x2ZXIuaW8vMS4wL2lkZW50aWZpZXJzL2RpZDpldGhyOjB4QjA3RWFkOTcxN2I0NEI2Y0Y0MzljNDc0MzYyYjlCMDg3N0NCQkY4Mzt0cmFuc2Zvcm0ta2V5cz1qd2tzIiwiaWRfdG9rZW5fc2lnbmVkX3Jlc3BvbnNlX2FsZyI6WyJFUzI1NksiLCJFZERTQSIsIlJTMjU2Il19fQ.LI6KBAAVAdLRL4pfYPYbRcz1pKJ5WKVxgUp7eXntC1J6OT1WqegFqdgONshNIXXdULUhxy1C0paXc85Kgd9bWA'
-}
-
-const requestGoodEmbeddedJWT = 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request=' + jwt;
-const requestGoodUriJWT = 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request_uri=' + jwt_uri;
-
-const requestBadProtocol = 'opend://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request=' + jwt;
-const requestBadNoSlashes = 'openid:?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request=' + jwt;
-const requestBadNoResponseType = 'openid://?response_tye=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request=' + jwt;
-const requestBadIncorrectResponseType = 'openid://?response_type=id_toke&client_id=https://rp.example.com/cb&scope=openid did_authn&request=' + jwt;
-const requestBadNoClientId = 'openid://?response_type=id_token&client_i=https://rp.example.com/cb&scope=openid did_authn&request=' + jwt;
-const requestBadNoScope = 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openi did_authn&request=' + jwt;
-const requestBadNoScopeOpenId = 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=did_authn&request=' + jwt;
-const requestBadNoScopeDidAuthN = 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid&request=' + jwt;
-const requestBadNoJWT = 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request=';
-const requestBadNoJWTUri = 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request_uri=';
-const requestBadIncorrectJWTUri = 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request_uri=http://localhost/incorrectRequestJWT';
-
-const badRequestError = new Error('Bad request');
-const noJWTError = new Error('Cannot resolve request jwt');
 
 describe("Validator -> To test request validation", function () {
     test("Request parsing", async () => {
-        let parsed = validator.parseRequest(requestGoodEmbeddedJWT);
+        let parsed = validator.parseRequest(resources.requests.good.requestGoodEmbeddedJWT);
         expect(parsed.slashes).toBeTruthy();
         expect(parsed.protocol).toEqual('openid:');
-        expect(parsed.query).toEqual(queryObj);
+        expect(parsed.query).toEqual(resources.queryObj);
     });
     describe("Request params validation", function (){
         beforeAll(()=>{
-            nock('http://localhost').get('/requestJWT').reply(200, jwt).get('/incorrectRequestJWT').reply(404,'Not found');
+            nock('http://localhost').get('/requestJWT').reply(200, resources.jwts.jwtGoodEncoded).get('/incorrectRequestJWT').reply(404,'Not found');
         });
 
         test('Request params validation - expect truthy', async () => {
-            let returnedJWT = await validator.validateRequestParams(requestGoodEmbeddedJWT);
-            expect(returnedJWT).toEqual(jwt);
+            let returnedJWT = await validator.validateRequestParams(resources.requests.good.requestGoodEmbeddedJWT);
+            expect(returnedJWT).toEqual(resources.jwts.jwtGoodEncoded);
 
             returnedJWT = undefined;
-            returnedJWT = await validator.validateRequestParams(requestGoodUriJWT);
-            expect(returnedJWT).toEqual(jwt);
+            returnedJWT = await validator.validateRequestParams(resources.requests.good.requestGoodUriJWT);
+            expect(returnedJWT).toEqual(resources.jwts.jwtGoodEncoded);
         });
 
         test('Request params validation - expect falsy', async () => {
-            let validityPromise = validator.validateRequestParams(requestBadProtocol);
-            await expect(validityPromise).rejects.toEqual(badRequestError);
+            let validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadProtocol);
+            await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(requestBadNoSlashes);
-            await expect(validityPromise).rejects.toEqual(badRequestError);
+            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoSlashes);
+            await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(requestBadNoResponseType);
-            await expect(validityPromise).rejects.toEqual(badRequestError);
+            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoResponseType);
+            await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(requestBadIncorrectResponseType);
-            await expect(validityPromise).rejects.toEqual(badRequestError);
+            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadIncorrectResponseType);
+            await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(requestBadNoClientId);
-            await expect(validityPromise).rejects.toEqual(badRequestError);
+            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoClientId);
+            await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(requestBadNoScope);
-            await expect(validityPromise).rejects.toEqual(badRequestError);
+            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoScope);
+            await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(requestBadNoScopeOpenId);
-            await expect(validityPromise).rejects.toEqual(badRequestError);
+            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoScopeOpenId);
+            await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(requestBadNoScopeDidAuthN);
-            await expect(validityPromise).rejects.toEqual(badRequestError);
+            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoScopeDidAuthN);
+            await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(requestBadNoJWT);
-            await expect(validityPromise).rejects.toEqual(noJWTError);
+            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoJWT);
+            await expect(validityPromise).rejects.toEqual(resources.errors.noJWTError);
 
-            validityPromise = validator.validateRequestParams(requestBadNoJWTUri);
-            await expect(validityPromise).rejects.toEqual(noJWTError);
+            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoJWTUri);
+            await expect(validityPromise).rejects.toEqual(resources.errors.noJWTError);
 
-            validityPromise = validator.validateRequestParams(requestBadIncorrectJWTUri);
-            await expect(validityPromise).rejects.toEqual(noJWTError);
+            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadIncorrectJWTUri);
+            await expect(validityPromise).rejects.toEqual(resources.errors.noJWTError);
         });
     });
+    
    
 });
---
+++ test/validator.test.resources.js
@@ -0,0 +1,87 @@
+const JWT = require('../src/common/jwt');
+const { ERRORS } = require('../src/common/validator');
+const { generateKeyPairSync } = require('crypto');
+
+const jwtGoodDecoded = {
+    header: {
+        "typ": "JWT",
+        "alg": "ES256K",
+        "kid": "did:example:0xab#key-1"
+    },
+    payload: {
+        "iss": "did:example:0xab",
+        "response_type": "id_token",
+        "client_id": "https://my.rp.com/cb",
+        "scope": "openid did_authn",
+        "state": "af0ifjsldkj",
+        "nonce": "n-0S6_WzA2Mj",
+        "response_mode": "form_post",
+        "registration": {
+            "jwks_uri": "https://uniresolver.io/1.0/identifiers/did:example:0xab;transform-keys=jwks",
+            "id_token_signed_response_alg": ["ES256K", "EdDSA", "RS256"]
+        }
+    }
+}
+
+const keyPair = generateKeyPairSync('rsa', {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem'
+    },
+    privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem'
+    }
+});
+
+const jwtGoodEncoded = JWT.signRS256(jwtGoodDecoded.header, jwtGoodDecoded.payload, keyPair.privateKey);
+const jwt_uri = 'http://localhost/requestJWT';
+
+const jwts = {
+    jwtGoodDecoded,
+    jwtGoodEncoded,
+    bad:{
+
+    }
+}
+
+const queryObj = {
+    response_type: 'id_token',
+    client_id: 'https://rp.example.com/cb',
+    scope: 'openid did_authn',
+    request: jwtGoodEncoded
+}
+
+const requests = {
+    good: {
+        requestGoodEmbeddedJWT : 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request=' + jwtGoodEncoded,
+        requestGoodUriJWT : 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request_uri=' + jwt_uri
+    },
+    bad:{
+        requestBadProtocol : 'opend://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request=' + jwtGoodEncoded,
+        requestBadNoSlashes : 'openid:?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request=' + jwtGoodEncoded,
+        requestBadNoResponseType : 'openid://?response_tye=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request=' + jwtGoodEncoded,
+        requestBadIncorrectResponseType : 'openid://?response_type=id_toke&client_id=https://rp.example.com/cb&scope=openid did_authn&request=' + jwtGoodEncoded,
+        requestBadNoClientId : 'openid://?response_type=id_token&client_i=https://rp.example.com/cb&scope=openid did_authn&request=' + jwtGoodEncoded,
+        requestBadNoScope : 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openi did_authn&request=' + jwtGoodEncoded,
+        requestBadNoScopeOpenId : 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=did_authn&request=' + jwtGoodEncoded,
+        requestBadNoScopeDidAuthN : 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid&request=' + jwtGoodEncoded,
+        requestBadNoJWT : 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request=',
+        requestBadNoJWTUri : 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request_uri=',
+        requestBadIncorrectJWTUri : 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request_uri=http://localhost/incorrectRequestJWT',
+
+    }
+}
+
+const errors = {
+    badRequestError : new Error(ERRORS.BAD_REQUEST_ERROR),
+    noJWTError : new Error(ERRORS.JWT_RESOLVE_ERROR),
+}
+
+module.exports = {
+    jwts,
+    queryObj,
+    requests,
+    errors
+}
