a8fdee37071b0cc77c651bf6c1c0adf82ed78fe9
SanujaLW <sanujalw@gmail.com>
2020-04-05 17:42:04 +0530

    JWK - Added ES256 key retreival

--- src/common/jwk.js
+++ src/common/jwk.js
@@ -1,10 +1,12 @@
 const NodeRSA = require('node-rsa');
+var EC = require('elliptic').ec;
 const base64url = require('base64url');
 
 const ERRORS = Object.freeze({
     INVALID_JWK: 'Invalid JWK',
     INCORRECT_ALGORITHM: 'Incorrect algorithm',
     INSUFFIENT_PARAMS: 'Insufficient parameters to retrieve key',
+    INCORRECT_CURVE: 'Incorrect curve',
 });
 
 const getRSA256PublicKeyPem = function(jwk){
@@ -30,7 +32,35 @@ const getRSA256PublicKeyPem = function(jwk){
     }
 }
 
+const getES256PublicKeyHex = function(jwk){
+    if (jwk !== undefined) {
+        if (jwk.alg == 'ES256') {
+            if(jwk.crv === 'secp256k1'){
+                if (jwk.x && jwk.y) {
+                    let ec = new EC('secp256k1');
+
+                    let pub = {
+                        x: base64url.decode(jwk.x, 'hex'),
+                        y: base64url.decode(jwk.y, 'hex')
+                    }
+                    return ec.keyFromPublic(pub).getPublic().encode('hex');
+                } else {
+                    throw new Error(ERRORS.INSUFFIENT_PARAMS);
+                }
+            }
+            else{
+                throw new Error(ERRORS.INCORRECT_CURVE);
+            }
+        } else {
+            throw new Error(ERRORS.INCORRECT_ALGORITHM);
+        }
+    } else {
+        throw new Error(ERRORS.INVALID_JWK);
+    }
+}
+
 module.exports = {
     getRSA256PublicKeyPem,
+    getES256PublicKeyHex,
     ERRORS
 }
--- test/jwk.test.js
+++ test/jwk.test.js
@@ -1,5 +1,6 @@
 const JWK = require('../src/common/jwk');
 const NodeRSA = require('node-rsa');
+var EC = require('elliptic').ec;
 const base64url = require('base64url');
 
 describe("JWK key retrieval", function () {
@@ -18,7 +19,26 @@ describe("JWK key retrieval", function () {
         let key = new NodeRSA();
         key.importKey(pem, 'pkcs8-public-pem');
         let n = base64url.encode(key.keyPair.n.toBuffer().slice(1));
-        
+
         expect(n).toEqual(rsaJWK.n);
+    });
+    test("ES256", async () => {
+        let ec256JWK = {
+            "kty": "EC",
+            "use": "sig",
+            "crv": "secp256k1",
+            "kid": "789456",
+            "x": "vA1MdssF25DZIeLSd-SZBuzJvp1pVPZErBEGTnmZD_k",
+            "y": "OhW9OTHWEzZRFfRaucIvOQ5za-I2qI1u3i8rOx38ANw",
+            "alg": "ES256"
+        }
+
+        let hex = JWK.getES256PublicKeyHex(ec256JWK);
+
+        let ec = EC('secp256k1');
+        let key = ec.keyFromPublic(hex, 'hex');
+
+        expect(base64url.encode(key.getPublic().getX().toBuffer())).toEqual(ec256JWK.x);
+        expect(base64url.encode(key.getPublic().getY().toBuffer())).toEqual(ec256JWK.y);
     });
 });
