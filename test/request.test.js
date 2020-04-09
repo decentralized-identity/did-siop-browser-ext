63e7758684708957c50abc5e378d47f7b693347b
SanujaLW <sanujalw@gmail.com>
2020-04-10 01:58:40 +0530

    Request - Changed test import name validator -> request

--- test/request.test.js
+++ test/request.test.js
@@ -1,11 +1,11 @@
-const validator = require('../src/common/request');
+const request = require('../src/common/request');
 const nock = require('nock');
 const resources = require('./request.test.resources');
 
 
 describe("Validator -> To test request validation", function () {
     test("Request parsing", async () => {
-        let parsed = validator.parseRequest(resources.requests.good.requestGoodEmbeddedJWT);
+        let parsed = request.parseRequest(resources.requests.good.requestGoodEmbeddedJWT);
         expect(parsed.slashes).toBeTruthy();
         expect(parsed.protocol).toEqual('openid:');
         expect(parsed.query).toEqual(resources.queryObj);
@@ -16,53 +16,53 @@ describe("Validator -> To test request validation", function () {
         });
 
         test('Request params validation - expect truthy', async () => {
-            let returnedJWT = await validator.validateRequestParams(resources.requests.good.requestGoodEmbeddedJWT);
+            let returnedJWT = await request.validateRequestParams(resources.requests.good.requestGoodEmbeddedJWT);
             expect(returnedJWT).toEqual(resources.jwts.jwtGoodEncoded);
 
             returnedJWT = undefined;
-            returnedJWT = await validator.validateRequestParams(resources.requests.good.requestGoodUriJWT);
+            returnedJWT = await request.validateRequestParams(resources.requests.good.requestGoodUriJWT);
             expect(returnedJWT).toEqual(resources.jwts.jwtGoodEncoded);
         });
 
         test('Request params validation - expect falsy', async () => {
-            let validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadProtocol);
+            let validityPromise = request.validateRequestParams(resources.requests.bad.requestBadProtocol);
             await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoSlashes);
+            validityPromise = request.validateRequestParams(resources.requests.bad.requestBadNoSlashes);
             await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoResponseType);
+            validityPromise = request.validateRequestParams(resources.requests.bad.requestBadNoResponseType);
             await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadIncorrectResponseType);
+            validityPromise = request.validateRequestParams(resources.requests.bad.requestBadIncorrectResponseType);
             await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoClientId);
+            validityPromise = request.validateRequestParams(resources.requests.bad.requestBadNoClientId);
             await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoScope);
+            validityPromise = request.validateRequestParams(resources.requests.bad.requestBadNoScope);
             await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoScopeOpenId);
+            validityPromise = request.validateRequestParams(resources.requests.bad.requestBadNoScopeOpenId);
             await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoScopeDidAuthN);
+            validityPromise = request.validateRequestParams(resources.requests.bad.requestBadNoScopeDidAuthN);
             await expect(validityPromise).rejects.toEqual(resources.errors.badRequestError);
 
-            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoJWT);
+            validityPromise = request.validateRequestParams(resources.requests.bad.requestBadNoJWT);
             await expect(validityPromise).rejects.toEqual(resources.errors.noJWTError);
 
-            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadNoJWTUri);
+            validityPromise = request.validateRequestParams(resources.requests.bad.requestBadNoJWTUri);
             await expect(validityPromise).rejects.toEqual(resources.errors.noJWTError);
 
-            validityPromise = validator.validateRequestParams(resources.requests.bad.requestBadIncorrectJWTUri);
+            validityPromise = request.validateRequestParams(resources.requests.bad.requestBadIncorrectJWTUri);
             await expect(validityPromise).rejects.toEqual(resources.errors.noJWTError);
         });
     });
     describe("Request jwt validation", function(){
         test("Request jwt validation - expect truthy", async ()=>{
             try {
-                let validity = await validator.validateRequestJWT(resources.jwts.jwtGoodEncoded);
+                let validity = await request.validateRequestJWT(resources.jwts.jwtGoodEncoded);
                 expect(validity).toBeTruthy();
             } catch (err) {
                 let errors = []
@@ -75,19 +75,19 @@ describe("Validator -> To test request validation", function () {
             }
         });
         test("Request jwt validation - expect falsy", async () => {
-            let validityPromise = validator.validateRequestJWT(resources.jwts.bad.jwtBadNoKid);
+            let validityPromise = request.validateRequestJWT(resources.jwts.bad.jwtBadNoKid);
             await expect(validityPromise).rejects.toEqual(resources.errors.malformedJWTError);
 
-            validityPromise = validator.validateRequestJWT(resources.jwts.bad.jwtBadNoIss);
+            validityPromise = request.validateRequestJWT(resources.jwts.bad.jwtBadNoIss);
             await expect(validityPromise).rejects.toEqual(resources.errors.malformedJWTError);
 
-            validityPromise = validator.validateRequestJWT(resources.jwts.bad.jwtBadNoScope);
+            validityPromise = request.validateRequestJWT(resources.jwts.bad.jwtBadNoScope);
             await expect(validityPromise).rejects.toEqual(resources.errors.malformedJWTError);
 
-            validityPromise = validator.validateRequestJWT(resources.jwts.bad.jwtBadIncorrectScope);
+            validityPromise = request.validateRequestJWT(resources.jwts.bad.jwtBadIncorrectScope);
             await expect(validityPromise).rejects.toEqual(resources.errors.malformedJWTError);
 
-            validityPromise = validator.validateRequestJWT(resources.jwts.bad.jwtBadNoRegistration);
+            validityPromise = request.validateRequestJWT(resources.jwts.bad.jwtBadNoRegistration);
             await expect(validityPromise).rejects.toEqual(resources.errors.malformedJWTError);
         });
     });
