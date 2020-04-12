653f3cce52c3dfd1c54a09c09ddae0afde243986
SanujaLW <sanujalw@gmail.com>
2020-04-12 07:30:33 +0530

    Response - Added generateResponse function

---
+++ src/common/response.js
@@ -0,0 +1,64 @@
+const { getKeyFromDidDoc } = require('./util');
+const { verifyKeyPair, signJWT } = require('./jwt');
+const { getJWK, getBase64UrlEncodedJWKThumbprint, getKeyTypeByAlgorithm } = require('./jwk');
+const etheruemPrivateKeyToPublicKey = require('ethereum-private-key-to-public-key');
+
+
+const ERRORS = Object.freeze({
+    UNSUPPORTED_ALGO: 'Algorithm not supported',
+    KEY_MISMATCH: 'Signing key does not match kid',
+});
+
+//requestPayload = payload of the request
+//signing = signing info { alg, kid, signing_key(private key),  }
+//me = end user info { did, did_doc } 
+const generateResponse = async function(requestPayload = {}, signing = {}, me = {}){
+    try {
+        let payload = {
+            iss: 'https://self-issued.me',
+        }
+    
+        let header = {
+            typ: 'JWT',
+        }
+    
+        if (requestPayload.registration.id_token_signed_response_alg.includes(signing.alg)){
+            header.alg = signing.alg;
+        }
+        else{
+            Promise.reject(ERRORS.UNSUPPORTED_ALGO);
+        }
+    
+        let publicKey = await getKeyFromDidDoc(me.did, signing.kid, me.did_doc);
+        if(verifyKeyPair(signing.signing_key, publicKey, signing.alg)){
+            header.kid = signing.kid;
+        }
+        else{
+            Promise.reject(ERRORS.KEY_MISMATCH);
+        }
+        if (signing.alg === 'ES256K-R') publicKey = etheruemPrivateKeyToPublicKey(signing.signing_key);
+
+        payload.did = me.did;
+        if(me.did_doc) payload.did_doc = me.did_doc;
+        if(requestPayload.client_id) payload.aud = requestPayload.client_id;
+
+        let kty = getKeyTypeByAlgorithm(signing.alg);
+        payload.sub_jwk = getJWK(publicKey, kty);
+        payload.sub = getBase64UrlEncodedJWKThumbprint(payload.sub_jwk);
+
+        if (requestPayload.nonce) payload.nonce = requestPayload.nonce;
+        if (requestPayload.state) payload.state = requestPayload.state;
+
+        payload.iat = Date.now();
+
+        return signJWT(header, payload, signing.signing_key);
+    } catch (err) {
+        Promise.reject(err);
+    }
+    
+
+}
+
+module.exports = {
+    generateResponse,
+}
---
+++ test/response.test.js
@@ -0,0 +1,34 @@
+const { generateResponse } = require('../src/common/response');
+const { verifyJWT } = require('../src/common/jwt');
+
+
+describe("Response -> To test response functions", function () {
+    test("Response generation - expect truthy", async () => {
+        let requestPayload = {
+            "iss": "did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83",
+            "response_type": "id_token",
+            "client_id": "https://my.rp.com/cb",
+            "scope": "openid did_authn",
+            "state": "af0ifjsldkj",
+            "nonce": "n-0S6_WzA2Mj",
+            "response_mode": "form_post",
+            "registration": {
+                "jwks_uri": "https://uniresolver.io/1.0/identifiers/did:example:0xab;transform-keys=jwks",
+                "id_token_signed_response_alg": ["ES256K", "ES256K-R", "EdDSA", "RS256"]
+            }
+        }
+        let signing = {
+            alg: 'ES256K-R',
+            kid: "did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner",
+            signing_key: 'CE438802C1F0B6F12BC6E686F372D7D495BC5AA634134B4A7EA4603CB25F0964',
+            public_key: '0xB07Ead9717b44B6cF439c474362b9B0877CBBF83',
+        }
+        let me = {
+            did: 'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83'
+        }
+
+        let response = await generateResponse(requestPayload, signing, me);
+
+        expect(verifyJWT(response, 'ES256K-R', signing.public_key)).toBeTruthy();
+    });
+});
