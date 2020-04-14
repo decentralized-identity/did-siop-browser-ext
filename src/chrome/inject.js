961d35c8264b00b8aa2e5c8aabe2f59f2c0bb659
SanujaLW <sanujalw@gmail.com>
2020-04-14 22:58:28 +0530

    Chrome - Removed inject.js

--- src/chrome/inject.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('did-siop-login').addEventListener('click',
-    function(){
-        var port = chrome.runtime.connect({name: "did-siop"});
-        port.postMessage(request);
-        port.onMessage.addListener(function(msg) {
-            console.log(msg);
-        });
-    }
-);
