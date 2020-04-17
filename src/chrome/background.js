c71140b9aea22f955d14dee1d75228686251b9fe
SanujaLW <sanujalw@gmail.com>
2020-04-17 15:30:15 +0530

    Chrome - Removed onStartup event binding

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -3,50 +3,48 @@ const { generateResponse } = require('../common/response');
 const ERROR_RESPONSES = require('../common/response.errors');
 const { encodeBase64Url } = require('../common/jwt');
 
-chrome.runtime.onStartup.addListener(function() {
-    chrome.tabs.onCreated.addListener(function(){
-        chrome.tabs.query({ active: true, lastFocusedWindow: true}, tabs => {
-            let request = tabs[0].pendingUrl;
-            if (parseRequest(request).url === 'openid://') {
-                if (confirm('Sign in using did-siop?')){
-                    validateRequest(request).then(decodedRequest => {
-                            generateResponse(decodedRequest.payload, signing, me).then(response => {
-                                    let uri = decodedRequest.payload.client_id + '#' + response;
-                                    chrome.tabs.update(tabs[0].id, {
-                                        url: uri,
-                                    });
-                                    console.log('Sent response to ' + decodedRequest.payload.client_id + ' with id_token: ' + response);
-                            })
-                            .catch(err => {
-                                alert(err);
-                            });
-                    })
-                    .catch(err => {
-                        let uri = parseRequest(request).query.client_id;
-                        if (uri) {
-                            uri = uri + '#' + encodeBase64Url(ERROR_RESPONSES[err.message].response);
-                            chrome.tabs.update(tabs[0].id, {
-                                url: uri,
-                            });
-                        } else {
-                            alert('Error: invalid redirect url');
-                        }
-                    });
-                }
-                else{
+chrome.tabs.onCreated.addListener(function(){
+    chrome.tabs.query({ active: true, lastFocusedWindow: true}, tabs => {
+        let request = tabs[0].pendingUrl;
+        if (parseRequest(request).url === 'openid://') {
+            if (confirm('Sign in using did-siop?')){
+                validateRequest(request).then(decodedRequest => {
+                        generateResponse(decodedRequest.payload, signing, me).then(response => {
+                                let uri = decodedRequest.payload.client_id + '#' + response;
+                                chrome.tabs.update(tabs[0].id, {
+                                    url: uri,
+                                });
+                                console.log('Sent response to ' + decodedRequest.payload.client_id + ' with id_token: ' + response);
+                        })
+                        .catch(err => {
+                            alert(err);
+                        });
+                })
+                .catch(err => {
                     let uri = parseRequest(request).query.client_id;
-                    if(uri){
-                        uri = uri + '#' + encodeBase64Url(ERROR_RESPONSES.access_denied.response);
+                    if (uri) {
+                        uri = uri + '#' + encodeBase64Url(ERROR_RESPONSES[err.message].response);
                         chrome.tabs.update(tabs[0].id, {
                             url: uri,
                         });
-                    }
-                    else{
+                    } else {
                         alert('Error: invalid redirect url');
                     }
+                });
+            }
+            else{
+                let uri = parseRequest(request).query.client_id;
+                if(uri){
+                    uri = uri + '#' + encodeBase64Url(ERROR_RESPONSES.access_denied.response);
+                    chrome.tabs.update(tabs[0].id, {
+                        url: uri,
+                    });
+                }
+                else{
+                    alert('Error: invalid redirect url');
                 }
             }
-        });
+        }
     });
 });
 
