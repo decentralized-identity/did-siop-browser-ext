fc1c8f3059d5302c4aa167441545a62f83654e60
SanujaLW <sanujalw@gmail.com>
2020-05-17 18:44:37 +0530

    Removed old files

--- src/common/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    resolverRpcUrls: {
-        ethr: "https://rinkeby.infura.io/v3/7063b610c6f34907b0dd0cdab07f397b"
-    }
-}
-
-module.exports = config;
--- test/resolver.test.js
+++
@@ -1,9 +0,0 @@
-const resolver = require('../src/common/resolver');
-
-describe("Resolver -> To test resolver", function () {
-    test("Resolve did document", async () => {
-        let doc = await resolver.resolve('did:ethr:0xc2396b75dc474cfcdd3475ed53884fe1d0dd4d4b');
-        expect(doc).toHaveProperty('@context');
-        expect(doc).toHaveProperty('id');
-    });
-});
