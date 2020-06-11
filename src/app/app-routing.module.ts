4200efb146da8c4f7cd96686cd8fdb07f7f0f33f
SanujaLW <59592997+SanujaLW@users.noreply.github.com>
2020-06-11 22:33:46 +0530

    Added user login with dummy password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'main', component: MainComponent}
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('password') password: ElementRef;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+
+  login(value){
+    if(value.length != 0){
+      if(value === '12345'){
+        this.router.navigate(['main']);
+      }
+      else{
+        this.password.nativeElement.value = '';
+        this.password.nativeElement.classList.add('invalid-password');
+        this.password.nativeElement.placeholder = 'Invalid password';
+      }
+    }
+    else{
+      this.password.nativeElement.placeholder = 'Please enter the password';
+      this.password.nativeElement.classList.add('invalid-password');
+    }
   }
 
 }
