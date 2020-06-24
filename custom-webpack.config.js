96bde09a39a2560239881d186c928640d993767e
SanujaLW <sanujalw@gmail.com>
2020-06-24 10:33:52 +0530

    Customized polyfill generation

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,6 +1,18 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: { 
         background: 'src/background.ts',
         inject: 'src/inject.ts'
     },
+    node: {
+        stream: true,
+        crypto: true,
+        global: false
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+          global: require.resolve('./global.js')
+        })
+    ],
 }
